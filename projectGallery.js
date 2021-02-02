ead4d9166114edb3b48f92fe16f3f30864250c85
hugoC42 <hugo.constant42@gmail.com>
2021-02-02 16:52:19 +0200

    Finished the styling and responsive CSS Corrected few minor issues

--- projectGallery.js
+++ projectGallery.js
@@ -27,64 +27,87 @@ function generateCardGallery(galleryElement, cardDatalist, type){
 
 function generateProjectCard(cardData, type)
 {
+    var element = $("<div> </div>");
+    element.addClass("p-3 col-lg-4 col-10 m-auto");
+
     var card = $("<div> </div>");
-    card.addClass("card bg-info shadow-sm my-3");
-    card.attr("style", "width:350px");
+    card.addClass("card bg-info shadow");
+    card.attr("style", "height:fit-content");
     
-    // Placeholder image
-    var image=$("<img src=\"" + cardData.img + "\" class=\"card-img-top\" alt=\"" + cardData.title + "\">");
-    image.attr("style", "height:180px");
+    // Image
+    var image=$("<img >");
+    image.addClass("card-img-top cardStyle__image");
+    image.attr("style", "height:250px");
+    image.attr("src", cardData.img);
+    image.attr("alt", cardData.title);
 
     // Card-body
     var cardBody =  $("<div> </div>");
     cardBody.addClass("card-body");
 
     var title = $("<h4> </h4>").text(cardData.title);
-    title.addClass("card-title");
+    title.addClass("card-title cardStyle__title");
+
     var desc = $("<p> </p>").text(cardData.desc);
-    desc.addClass("card-text");
+    desc.addClass("card-text cardStyle__desc");
  
     // TagList
     var tagList = $("<ul> </ul>");
-    tagList.addClass("list-inline");
+    tagList.addClass("list-inline text-center");
 
     for(let i=0; i<cardData.tags.length; i++){
         let tag= $("<li> </li>").text(cardData.tags[i]);
-        tag.addClass("list-inline-item skillTag");
+        tag.addClass("list-inline-item cardStyle__tag m-1 bg-secondary text-white");
         tagList.append(tag);
     }
 
     // Buttons
+    var buttonBar = $("<div> </div>");
+    buttonBar.addClass("text-center");
+
     var buttonView = $("<a target=\"_blank\"> </a>");
-    buttonView.addClass("btn btn-primary");
+    buttonView.addClass("btn btn-primary col-5 mr-3 cardStyle__button");
 
     if(type === "dev"){
         buttonView.text("Live");
         buttonView.attr("href", cardData.linkLive);
+        if(cardData.linkLive === "#")
+        {
+            buttonView.addClass("disabled");
+        }
     } 
     else if(type === "eng"){
         buttonView.text("Documentation");
         buttonView.attr("href", cardData.linkDocu);
+        if(cardData.linkDocu === "#")
+        {
+            buttonView.addClass("disabled");
+        }
     }
     else{
         alert("wrong project type entered");
     }
 
     var buttonCode = $("<a target=\"_blank\"> </a>");
-    buttonCode.addClass("btn btn-primary");
+    buttonCode.addClass("btn btn-primary col-5 cardStyle__button");
     buttonCode.text("Code");
     buttonCode.attr("href", cardData.linkCode);
 
+    buttonBar.append(buttonView);
+    buttonBar.append(buttonCode);
+
+    // Building
     cardBody.append(title);
     cardBody.append(desc);
     cardBody.append(tagList);
-    cardBody.append(buttonView);
-    cardBody.append(buttonCode);
+    cardBody.append(buttonBar);
 
     card.append(image);
     card.append(cardBody);
 
-    return card;
+    element.append(card);
+
+    return element;
 }
 
 ///////// Project card HTML code
